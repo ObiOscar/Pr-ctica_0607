879824e7092dddfb95a31465f972909ef3c9f056
ObiOscar <oscar.fer.rodz@gmail.com>
2016-10-16 18:37:15 +0200

    2º Modificación, muestra lugar de nacimiento https://github.com/pro016/pro016/blob/master/actividades/0607.md

--- practica1_0495.java
+++ practica1_0495.java
@@ -4,5 +4,6 @@ public class practica1_0495 {
 	public static void main(String args[]){
 		System.out.println("Nombre alumno: Oscar Fernandez Rodriguez");
 		System.out.println("Edad alumno: 24 años");
+		System.out.println("Nacido en: León");
 	}
 }
