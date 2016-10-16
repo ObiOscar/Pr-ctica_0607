327ec53a0528908aa7a2d0c249231b8bd5d47ddd
ObiOscar <oscar.fer.rodz@gmail.com>
2016-10-16 18:35:19 +0200

    1º Modificación, muestra edad https://github.com/pro016/pro016/blob/master/actividades/0607.md

--- practica1_0495.java
+++ practica1_0495.java
@@ -3,5 +3,6 @@ public class practica1_0495 {
 	
 	public static void main(String args[]){
 		System.out.println("Nombre alumno: Oscar Fernandez Rodriguez");
+		System.out.println("Edad alumno: 24 años");
 	}
 }
